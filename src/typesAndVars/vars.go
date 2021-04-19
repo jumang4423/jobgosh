4d2228ede70be3adf3cb3ef2a05936ac42717487
ryoma okuda <catontheskate4423@gmail.com>
2021-04-19 13:42:45 +0900

    version 0.2 to 0.21

--- src/typesAndVars/vars.go
+++ src/typesAndVars/vars.go
@@ -2,7 +2,7 @@ package types
 
 var (
 	DOCKER_DEVELOPMENT        = false
-	Version            string = "v0.2"
+	Version            string = "v0.21"
 	Revision           string = "17/04/2021"
 	LogPath            string = "log/"
 	GroupPath          string = "group/"
