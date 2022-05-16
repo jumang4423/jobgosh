8886a4e233bbdc2fd23d5fd7e7fe5bd9e61afb42
jumang4423 <catontheskate4423@gmail.com>
2022-05-16 16:04:44 +0900

    nixでも起動できる

--- shell.nix
+++ shell.nix
@@ -1,7 +1,21 @@
-with import <nixpkgs> {}; with goPackages;
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/release-21.11.tar.gz") { } }:
 
-buildGoPackage rec {
-  name = "jobgosh";
-  buildInputs = [ net osext ];
-  goPackagePath = "github.com/jumang4423/jobgosh";
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [
+    nixpkgs-fmt
+    rnix-lsp
+    docker-client
+    gnumake
+
+    # go development
+    go
+    go-outline
+    gopls
+    gopkgs
+    go-tools
+    delve
+  ];
+
+  hardeningDisable = [ "all" ];
 }
